e9f74501b0be1f14f35e1050ce7f1ea38588dcad
zhimin <zshao14@jh.edu>
2025-03-23 14:25:14 -0400

    megadepth_all

--- mast3r/dust3r/dust3r/datasets/megadepth.py
+++ mast3r/dust3r/dust3r/datasets/megadepth.py
@@ -96,7 +96,7 @@ class MegaDepth(BaseStereoViewDataset):
         return views
 
 class MegaDepth_all(BaseStereoViewDataset):
-    def __init__(self, *args, split, ROOT, min_overlap=0.0, max_overlap=0.0, max_num_pairs = 100_000, **kwargs):
+    def __init__(self, *args, split, ROOT, min_overlap=0.0, max_overlap=1.0, max_num_pairs = 100_000, **kwargs):
         self.ROOT = ROOT
         super().__init__(*args, **kwargs)
 
