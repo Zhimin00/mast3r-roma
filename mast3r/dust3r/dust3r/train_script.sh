3ebe1f046d1947ebf6ff2609af81e9ac42ce7e29
zhimin <zshao14@jh.edu>
2025-04-01 15:17:32 -0400

    resnet

--- mast3r/dust3r/dust3r/train_script.sh
+++ mast3r/dust3r/dust3r/train_script.sh
@@ -110,7 +110,7 @@ torchrun --nproc_per_node 4 train.py \
     --test_dataset "Habitat(80_000, ROOT='/home/cpeng26/scratchrchella4/data/habitat_processed', split='val', resolution=224, seed=777) + 1_000 @ BlendedMVS(split='val', ROOT='/home/cpeng26/scratchrchella4/data/blendedmvs_processed', resolution=224, seed=777) + 1_000 @ MegaDepth(split='val', ROOT='/home/cpeng26/scratchrchella4/data/megadepth_dataset_processed', resolution=224, seed=777) + 1_000 @ Co3d(split='test', ROOT='/home/cpeng26/scratchrchella4/data/co3d_subset_processed', mask_bg='rand', resolution=224, seed=777)" \
     --train_criterion="ConfLoss(Regr3D(L21, norm_mode='avg_dis'), alpha=0.2)" \
     --test_criterion="Regr3D_ScaleShiftInv(L21, gt_scale=True)" \
-    --model="AsymmetricCroCo3DStereo_DINOv2_frozen(pos_embed='RoPE100', img_size=(224, 224), patch_size=14, head_type='linear', output_mode='pts3d', depth_mode=('exp', -inf, inf), conf_mode=('exp', 1, inf), enc_embed_dim=1024, enc_depth=24, enc_num_heads=16, dec_embed_dim=768, dec_depth=12, dec_num_heads=12)" \
+    --model="AsymmetricCroCo3DStereo_DINOv2(pos_embed='RoPE100', img_size=(224, 224), patch_size=14, head_type='linear', output_mode='pts3d', depth_mode=('exp', -inf, inf), conf_mode=('exp', 1, inf), enc_embed_dim=1024, enc_depth=24, enc_num_heads=16, dec_embed_dim=768, dec_depth=12, dec_num_heads=12)" \
     --pretrained="checkpoints/CroCo_V2_ViTLarge_BaseDecoder.pth" \
     --lr=0.0001 --min_lr=1e-06 --warmup_epochs=10 --epochs=100 --batch_size=64 --accum_iter=1 \
     --save_freq=5 --keep_freq=10 --eval_freq=1  --num_workers=16 \
--- mast3r/mast3r/model.py
+++ mast3r/mast3r/model.py
@@ -12,7 +12,7 @@ from mast3r.catmlp_dpt_head import mast3r_head_factory
 import torchvision.models as tvm
 
 import mast3r.utils.path_to_dust3r  # noqa
-from dust3r.model import AsymmetricCroCo3DStereo, AsymmetricCroCo3DStereo_VGG # noqa
+from dust3r.model import AsymmetricCroCo3DStereo # noqa
 from dust3r.utils.misc import transpose_to_landscape  # noqa
 
 
@@ -69,36 +69,6 @@ class AsymmetricMASt3R(AsymmetricCroCo3DStereo):
         self.head2 = transpose_to_landscape(self.downstream_head2, activate=landscape_only)
 
 
-class AsymmetricMASt3R_VGG(AsymmetricCroCo3DStereo_VGG):
-    def __init__(self, desc_mode=('norm'), two_confs=False, desc_conf_mode=None, **kwargs):
-        self.desc_mode = desc_mode
-        self.two_confs = two_confs
-        self.desc_conf_mode = desc_conf_mode
-        self.vgg = VGG19()
-        super().__init__(**kwargs)
-
-    @classmethod
-    def from_pretrained(cls, pretrained_model_name_or_path, **kw):
-        if os.path.isfile(pretrained_model_name_or_path):
-            return load_model(pretrained_model_name_or_path, device='cpu')
-        else:
-            return super(AsymmetricMASt3R, cls).from_pretrained(pretrained_model_name_or_path, **kw)
-
-    def set_downstream_head(self, output_mode, head_type, landscape_only, depth_mode, conf_mode, patch_size, img_size, **kw):
-        assert img_size[0] % patch_size == 0 and img_size[
-            1] % patch_size == 0, f'{img_size=} must be multiple of {patch_size=}'
-        self.output_mode = output_mode
-        self.head_type = head_type
-        self.depth_mode = depth_mode
-        self.conf_mode = conf_mode
-        if self.desc_conf_mode is None:
-            self.desc_conf_mode = conf_mode
-        # allocate heads
-        self.downstream_head1 = mast3r_head_factory(head_type, output_mode, self, has_conf=bool(conf_mode))
-        self.downstream_head2 = mast3r_head_factory(head_type, output_mode, self, has_conf=bool(conf_mode))
-        # magic wrapper
-        self.head1 = transpose_to_landscape(self.downstream_head1, activate=landscape_only)
-        self.head2 = transpose_to_landscape(self.downstream_head2, activate=landscape_only)
 
 
 
