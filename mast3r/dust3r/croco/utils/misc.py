9af3ccbeb208fcfdac09bc9d2974a152fa48545c
zhimin <zshao14@jh.edu>
2025-04-21 16:48:54 -0400

    warp loss

--- mast3r/dust3r/croco/utils/misc.py
+++ mast3r/dust3r/croco/utils/misc.py
@@ -250,20 +250,19 @@ class NativeScalerWithGradNormCount:
         self._scaler = torch.cuda.amp.GradScaler(enabled=enabled)
 
     def __call__(self, loss, optimizer, clip_grad=None, parameters=None, create_graph=False, update_grad=True):
-        with torch.autograd.detect_anomaly():
-            self._scaler.scale(loss).backward(create_graph=create_graph)
-            if update_grad:
-                if clip_grad is not None:
-                    assert parameters is not None
-                    self._scaler.unscale_(optimizer)  # unscale the gradients of optimizer's assigned params in-place
-                    norm = torch.nn.utils.clip_grad_norm_(parameters, clip_grad)
-                else:
-                    self._scaler.unscale_(optimizer)
-                    norm = get_grad_norm_(parameters)
-                self._scaler.step(optimizer)
-                self._scaler.update()
+        self._scaler.scale(loss).backward(create_graph=create_graph)
+        if update_grad:
+            if clip_grad is not None:
+                assert parameters is not None
+                self._scaler.unscale_(optimizer)  # unscale the gradients of optimizer's assigned params in-place
+                norm = torch.nn.utils.clip_grad_norm_(parameters, clip_grad)
             else:
-                norm = None
+                self._scaler.unscale_(optimizer)
+                norm = get_grad_norm_(parameters)
+            self._scaler.step(optimizer)
+            self._scaler.update()
+        else:
+            norm = None
         return norm
 
     def state_dict(self):
