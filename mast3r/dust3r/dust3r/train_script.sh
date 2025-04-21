fde4ea1a02af7e7e9770c47eb21e9a0efc64ef73
zhimin <zshao14@jh.edu>
2025-04-21 15:13:02 -0400

    loss

--- mast3r/dust3r/dust3r/train_script.sh
+++ mast3r/dust3r/dust3r/train_script.sh
@@ -493,7 +493,7 @@ torchrun --nproc_per_node=4 train_warp.py \
     --model "AsymmetricMASt3R_warp(pos_embed='RoPE100', patch_embed_cls='ManyAR_PatchEmbed_cnn', cnn_type='vgg', img_size=(512, 512), head_type='warp+dpt', output_mode='pts3d', depth_mode=('exp', -inf, inf), conf_mode=('exp', 1, inf), enc_embed_dim=1024, enc_depth=24, enc_num_heads=16, dec_embed_dim=768, dec_depth=12, dec_num_heads=12)" \
     --train_criterion "ConfLoss(Regr3D(L21, norm_mode='?avg_dis', loss_in_log=False), alpha=0.2)"  \
     --test_criterion "Regr3D(L21, norm_mode='?avg_dis', gt_scale=True, sky_loss_value=0)" \
-    --pretrained "/home/cpeng26/scratchrchella4/checkpoints/MASt3R_ViTLarge_BaseDecoder_512_catmlpdpt_metric.pth" \
+    --pretrained "/home/cpeng26/scratchrchella4/checkpoints/DUSt3R_ViTLarge_BaseDecoder_512_dpt.pth" \
     --lr 0.0001 --min_lr 1e-06 --warmup_epochs 8 --epochs 50 --batch_size 3 --accum_iter 3 \
     --save_freq 1 --keep_freq 5 --eval_freq 1 --print_freq=10 --disable_cudnn_benchmark \
     --output_dir "/home/cpeng26/scratchrchella4/checkpoints/MASt3R_ViTLarge_BaseDecoder_512_warpdpt_0421"
